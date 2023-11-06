5be04fcb6b5483d81d4c3c83b54c2efc90944bda
Ellis Lunnon <ellislunnon@gmail.com>
2023-11-06 09:06:03 +0000

    Updated server a little

--- bin/server/main.go
+++ bin/server/main.go
@@ -13,26 +13,37 @@ type FormControls struct {
 	TrapNumbers []int
 }
 
+func ReadTemplate(data any, files ...string) string {
+	out := new(bytes.Buffer)
+	t := template.Must(
+		template.ParseFiles(files...),
+	)
+	t.Execute(out, data)
+	return out.String()
+}
+
 func main() {
-	log.Print("Hello, world")
 	e := echo.New()
+	e.Logger.Info("Starting server")
 	e.Static("/", "public")
 	e.GET("/", func(c echo.Context) error {
-		e.Logger.Print("Request received")
-		templ := template.Must(template.ParseFiles("templates/index.html", "templates/common.html"))
-		out := new(bytes.Buffer)
-		templ.Execute(out, FormControls{
-			TrapNumbers: []int{1, 2, 3, 4, 5, 6},
-		})
-		return c.HTML(http.StatusOK, out.String())
+		c.Logger().Debug("Index request received")
+		html := ReadTemplate(
+			FormControls{TrapNumbers: []int{1, 2, 3, 4, 5, 6}},
+			"templates/index.html",
+			"templates/common.html",
+		)
+		return c.HTML(http.StatusOK, html)
 	})
 
 	e.POST("/submit", func(c echo.Context) error {
-		e.Logger.Print("Submission received")
-		templ := template.Must(template.ParseFiles("templates/results.html", "templates/common.html"))
-		out := new(bytes.Buffer)
-		templ.Execute(out, nil)
-		return c.HTML(http.StatusOK, out.String())
+		c.Logger().Debug("Submit request received")
+		html := ReadTemplate(
+			nil,
+			"templates/results.html",
+			"templates/common.html",
+		)
+		return c.HTML(http.StatusOK, html)
 	})
 
 	log.Fatal(e.Start(":3000"))
