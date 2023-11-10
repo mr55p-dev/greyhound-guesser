26f65e354ca3049cabf70608ef707a27948750ed
Ellis Lunnon <ellislunnon@gmail.com>
2023-11-10 07:24:30 +0000

    Fix: parse odds as float not int

--- src/server.py
+++ src/server.py
@@ -23,11 +23,12 @@ class Model():
         network.load_state_dict(torch.load(weight_path, map_location=self.device))
         network.to(self.device)
         self.model = network
+        self.model_name = weight_path
 
     def sanitize(self, form):
         for field in form:
             if "odds" in field:
-                form[field] = 1 / int(form[field])
+                form[field] = 1 / float(form[field])
             elif "distance" in field:
                 form[field] = int(form[field]) / 1000
             elif "finished" in field:
@@ -37,9 +38,6 @@ class Model():
         return form
 
     def predict(self, form):
-        # app.logger.debug("Initial form")
-        # for field in form:
-        #     app.logger.debug(f"{field}: {form[field]} (type {type(form[field])})")
         form = self.sanitize(form)
         input = np.zeros((19, 1), dtype=np.float32)
         for idx, field in enumerate(fields):
@@ -62,7 +60,7 @@ app.logger.info("Started app")
 
 @app.route("/health", methods=["GET"])
 def health():
-    return "ok"
+    return f"Ok. Using model `{model.model_name}`."
 
 @app.route("/predict", methods=["POST"])
 def predict():
